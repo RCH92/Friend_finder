60002af4437790864431aacde4255d4173709e8a
Hallenbeck <RCHallen92@gmail.com>
2019-01-07 04:59:51 -0700

    third push

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -5,13 +5,13 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
        
         res.json(userData);
-        
+
     });
 
     app.post("/api/friends", function(req, res) {
         
         userData.push(req.body);
-        res.json(true);
+        res.json(userData);
         
     });
     
