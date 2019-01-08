a969e0cc92fe67c8a7ea97158e213e435b53ac03
Hallenbeck <RCHallen92@gmail.com>
2019-01-08 07:04:09 -0700

    final version

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,23 @@
-var users = []
+var users = [
+{
+image: "https://pixel.nymag.com/imgs/daily/vulture/2017/02/07/07-john-wick-2-2.w700.h700.jpg",
+name: "John Wick",
+survey: [1, 2, 4, 4, 5, 1, 5, 2, 5, 1]
+},
+{
+    image:"https://i.kinja-img.com/gawker-media/image/upload/s--KXrM58o6--/c_scale,f_auto,fl_progressive,q_80,w_800/ojbnostlfgjnwvcwq3gz.jpg",
+    name: "Austin Powers",
+    survey: [2, 1, 4, 2, 4, 3, 5, 4, 3, 5]
+},
+{
+    image: "https://static.tvtropes.org/pmwiki/pub/images/vincent_vega.jpg",
+    name: "Vincent Vega",
+    survey: [1, 2, 4, 2, 5, 1, 5, 5, 2, 2]
+},
+{
+    image: "https://images-na.ssl-images-amazon.com/images/I/41Xe%2B6VIgtL.jpg",
+    name: "Dexter",
+    survey: [5, 2, 1, 1, 1, 5, 1, 5, 1, 5]
+}
+]
 module.exports = users;
